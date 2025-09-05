d5c0ea2beb16f9d499cbb3aa6518261baddd8762
Seungmin9293 <tmdals92931@naver.com>
2025-09-05 16:15:11 +0900

    컨트롤러 초기

--- src/main/java/com/example/mobile_programming_final/dto/ExamScheduleDto.java
+++ src/main/java/com/example/mobile_programming_final/dto/ExamScheduleDto.java
@@ -1,5 +1,8 @@
 package com.example.mobile_programming_final.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +13,14 @@ import java.time.LocalDate;
 public class ExamScheduleDto {
 
     private Long id;
+
+    @NotBlank(message = "시험 이름 필수 항목입니다.")
     private String name;
+
+    @NotNull(message = "시험 날짜는 필수 항목입니다.")
+    @Future(message = "시험 날짜는 미래로 작성해야합니다.")
     private LocalDate examDate;
+
+    @NotNull(message = "등록 날짜는 필수 항목입니다.")
     private LocalDate registrationDate;
 }
